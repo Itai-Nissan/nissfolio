4f432dc0c58b5830eef612e7de877d54996d7eb8
Itai Nissan <itainissan@gmail.com>
2023-05-10 12:11:13 +0300

    details prllx and back button

--- src/views/work.details.vue
+++ src/views/work.details.vue
@@ -1,48 +1,62 @@
 <template>
-    <div class="bg-image">
-        <img src="https://res.cloudinary.com/counditai/image/upload/v1683704937/nissfolio/pexels-johannes-plenio-1110663-min_grlb3q.jpg" alt="">
-    </div>
-    <section v-if="currentWork" class="work-details container">
-        <div class="work-details-header">
-            <h3>{{ firstToCap(currentWork.category) }}</h3>
-            <h1>{{ firstToCap(currentWork.title) }}</h1>
-        </div>
-        <div class="work-details-tech">
-            <div class="specs card">
-                <div class="header">
-                    <h4>Tech specs</h4>
+    <div class="work-details-bg">
+        <section v-if="currentWork" class="work-details container">
+            <div class="work-details-header">
+                <div class="page-headline">
+                    <h3>{{ firstToCap(currentWork.category) }}</h3>
+                    <RouterLink to="/">
+                        <h5>Back</h5>
+                    </RouterLink>
                 </div>
-                <div class="tech-specs">
-                    <p>Frontend: <span>{{ firstToCap(currentWork.tech.frontend) }}</span></p>
-                    <p v-if="currentWork.tech.backend">Backend:<span>{{ firstToCap(currentWork.tech.backend) }}</span>
-                    </p>
-                    <p v-if="currentWork.tech.data">Data:<span>{{ firstToCap(currentWork.tech.data) }}</span></p>
-                </div>
+                <h1>{{ firstToCap(currentWork.title) }}</h1>
             </div>
-            <div class="specs-url card">
-                <div class="header">
-                    <h4>Go to</h4>
-                </div>
-                <div class="url">
-                    <a :href="currentWork.url" target="_blank">Site</a>
-                    <a :href="currentWork.git" target="_blank">Git</a>
-                </div>
-            </div>
-        </div>
-        <div class="work-details-media">
-            <section class="details-carousel">
-                <video v-if="currentWork.vidUrl" controls autoplay :src=currentWork.vidUrl></video>
-                <v-carousel show-arrowsshow-arrows-on-hover hide-delimiter-background height="auto">
+            <div class="work-details-wrapper">
 
-                    <v-carousel-item v-for="(img, i) in currentWork.images" :key="i"
-                        reverse-transition="fade-transition" transition="fade-transition">
-                        <a :href=img target="_blank"><v-img class="carousel-img" :src="img" alt=""></v-img></a>
-                        <v-img class="loader" v-if="img" src="https://res.cloudinary.com/counditai/image/upload/v1683703507/nissfolio/ajax-loading-icon-18_mfegpc.gif" alt=""></v-img>
-                    </v-carousel-item>
-                </v-carousel>
-            </section>
-        </div>
-    </section>
+                <div class="work-details-tech">
+                    <div class="specs card">
+                        <div class="header">
+                            <h4>Tech specs</h4>
+                        </div>
+                        <div class="tech-specs">
+                            <p>Frontend: <span>{{ firstToCap(currentWork.tech.frontend) }}</span></p>
+                            <p v-if="currentWork.tech.backend">Backend:<span>{{ firstToCap(currentWork.tech.backend)
+                            }}</span>
+                            </p>
+                            <p v-if="currentWork.tech.data">Data:<span>{{ firstToCap(currentWork.tech.data) }}</span></p>
+                        </div>
+                    </div>
+                    <div class="specs-url card">
+                        <div class="header">
+                            <h4>Go to</h4>
+                        </div>
+                        <div class="url">
+                            <a :href="currentWork.url" target="_blank">Site</a>
+                            <a :href="currentWork.git" target="_blank">Git</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="work-details-img">
+                    <v-parallax
+                        src="https://res.cloudinary.com/counditai/image/upload/v1683704937/nissfolio/pexels-johannes-plenio-1110663-min_grlb3q.jpg"></v-parallax>
+                </div>
+            </div>
+            <div class="work-details-media">
+                <section class="details-carousel">
+                    <video v-if="currentWork.vidUrl" controls autoplay :src=currentWork.vidUrl></video>
+                    <v-carousel show-arrowsshow-arrows-on-hover hide-delimiter-background height="auto">
+
+                        <v-carousel-item v-for="(img, i) in currentWork.images" :key="i"
+                            reverse-transition="fade-transition" transition="fade-transition">
+                            <a :href=img target="_blank"><v-img class="carousel-img" :src="img" alt=""></v-img></a>
+                            <v-img class="loader" v-if="img"
+                                src="https://res.cloudinary.com/counditai/image/upload/v1683703507/nissfolio/ajax-loading-icon-18_mfegpc.gif"
+                                alt=""></v-img>
+                        </v-carousel-item>
+                    </v-carousel>
+                </section>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
