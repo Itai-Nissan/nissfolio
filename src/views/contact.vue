586567a26bc5f6618adfff6300286d605c2bfaea
Itai Nissan <itainissan@gmail.com>
2023-05-10 11:24:50 +0300

    remove images+contact prllx

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -1,36 +1,39 @@
 <template>
     <section class="contact container">
         <h1>Contact</h1>
-        <section class="contact-info">
-            <div class="contact-card">
-                <mdicon name="linkedin" />
-                <a href="https://www.linkedin.com/in/itai-nissan-b4624821a/" target="_blank">Linkedin</a>
-            </div>
-            <div class="contact-card">
-                <mdicon name="github" />
-                <a href="https://github.com/Itai-Nissan/" target="_blank">Github</a>
-            </div>
-            <div class="contact-card email-card">
-                <div class="card-header">
-                    <mdicon name="email" />
-                    <span>itainissan@gmail.com</span>
+        <div class="contact-wrapper">
+            <section class="contact-info">
+                <div class="contact-card">
+                    <mdicon name="linkedin" />
+                    <a href="https://www.linkedin.com/in/itai-nissan-b4624821a/" target="_blank">Linkedin</a>
                 </div>
-                <form class="contact-form" action="#" @submit.prevent="onSendMail">
-                    <v-text-field v-model="name" class="name" type="text" placeholder="Your name.."></v-text-field>
-                    <v-text-field v-model="mail" class="email" type="email" placeholder="Your email.."></v-text-field>
-                    <v-textarea v-model="msg" cols="5" rows="5" placeholder="Your massege.."></v-textarea>
-                    <v-btn type="sumbit">Send</v-btn>
-                </form>
-            </div>
-            <transition name="fade">
-                <div v-if="visible">
-                    <v-alert>Mail sent, thank you for connecting</v-alert>
+                <div class="contact-card">
+                    <mdicon name="github" />
+                    <a href="https://github.com/Itai-Nissan/" target="_blank">Github</a>
                 </div>
-            </transition>
-        </section>
-        <v-parallax
-            src="https://res.cloudinary.com/counditai/image/upload/v1683704967/nissfolio/pexels-kyle-roxas-2138922-min_cys9kq.jpg"></v-parallax>
-
+                <div class="contact-card email-card">
+                    <div class="card-header">
+                        <mdicon name="email" />
+                        <span>itainissan@gmail.com</span>
+                    </div>
+                    <form class="contact-form" action="#" @submit.prevent="onSendMail">
+                        <v-text-field v-model="name" class="name" type="text" placeholder="Your name.."></v-text-field>
+                        <v-text-field v-model="mail" class="email" type="email" placeholder="Your email.."></v-text-field>
+                        <v-textarea v-model="msg" cols="5" rows="5" placeholder="Your massege.."></v-textarea>
+                        <v-btn type="sumbit">Send</v-btn>
+                    </form>
+                </div>
+                <transition name="fade">
+                    <div v-if="visible">
+                        <v-alert>Mail sent, thank you for connecting</v-alert>
+                    </div>
+                </transition>
+            </section>
+            <section class="contact-img">
+                <v-parallax
+                    src="https://res.cloudinary.com/counditai/image/upload/v1683704967/nissfolio/pexels-kyle-roxas-2138922-min_cys9kq.jpg"></v-parallax>
+            </section>
+        </div>
     </section>
 </template>
 
