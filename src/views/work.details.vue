0e31a9f2dcd68c6f089d0c2175b1203fd2acdf5d
Itai Nissan <itainissan@gmail.com>
2023-05-16 20:42:30 +0300

    details carousel image fixed height

--- src/views/work.details.vue
+++ src/views/work.details.vue
@@ -75,8 +75,7 @@ export default {
         const { id } = this.$route.params
         this.$store.dispatch({ type: 'getWorkById', id })
         this.onSetNavClass(this.navClass)
-        window.scrollTo(0, 0);
-
+        window.scrollTo(0, 0)
     },
     mounted() {
         this.currentWork = this.$store.getters.work
